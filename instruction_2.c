805042d50f74088bd407156b54126cb272acfc90
rekebuba <abukiabdullahi1@gmail.com>
2024-01-18 00:26:47 +0300

    monty

--- instruction_2.c
+++ instruction_2.c
@@ -46,20 +46,3 @@ void pop(stack_t **stack, unsigned int line)
 	}
 }
 
-/**
- * dlistint_len - returns the number of elements in a linked  list.
- * @h: double node
- * Return: size_t
- */
-size_t nod_len(stack_t **h)
-{
-	size_t count = 0;
-	const stack_t *temp = *h;
-
-	while (temp != NULL)
-	{
-		count++;
-		temp = temp->next;
-	}
-	return (count);
-}
